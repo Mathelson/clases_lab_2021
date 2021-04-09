9d2e826470f0498d84589f705dc6c3fd79989fdb
NelsonBermejo <nelsonbermejo@outlook.com.ar>
2021-04-09 19:26:09 -0300

    swap + ordena al reves

--- CLASE 7/clase7_0/main.c
+++ CLASE 7/clase7_0/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define TAM 5
+//4,3,6,5,1
 int main()
 {
-    printf("Hello world!\n");
+    int vec[TAM];
+
+
+    for(int i = 0; i < TAM; i++)
+        {
+            printf("ingrese un numero: ");
+            scanf("%d", &vec[i]);//*(vec + i); PUNTERO
+        }
+
+
+        for(int i = TAM - 1; i >= 0; i--)//Muestra al reves
+        {
+            printf("%d", vec[i]);
+        }
+        printf("\n\n");
+
     return 0;
 }
--- CLASE 7/clase7_1/main.c
+++ CLASE 7/clase7_1/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TAM 5
+/**SWAPEO*/
 int main()
 {
-    printf("Hello world\n");
+    //int array[TAM]={5,9,2,4,8};//ordenar de manera creciente 2,4,5,8,9
+    //elije dos elementos con una estrategia, quiero ordenar de menor a mayor
+    for(int i=0; i<3; i++){//1 hola x 10 chau
+        printf("Hola\n");
+        for(int j=0; j<5;j++)
+        {
+            printf("chau\n");
+        }
+    }
+
+
     return 0;
 }
