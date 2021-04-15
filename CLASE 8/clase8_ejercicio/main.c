25fc2c805e2cbe87b8e96b851c1be727a1d89567
NelsonBermejo <nelsonbermejo@outlook.com.ar>
2021-04-15 13:53:14 -0300

    corregir funciones con clase 8

--- CLASE 8/clase8_ejercicio/main.c
+++ CLASE 8/clase8_ejercicio/main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void miStrUpr(char cadena[]);
+int miStrlen(char cadena[]);
 int main()
 {
     printf("Hello world!\n");
     return 0;
+}
+int miStrlen(char cadena[])
+{
+    //contar la cantidad de caracteres
+    int contador=0;
+
+    //recorrro hasta el \0
+    for(int i=0; cadena[i] != '\0';i++)
+    {
+        contador++;
+
+    }
+    return contador;
+}
+void miStrUpr(char cadena[])
+{
+    int contador=0;
+
+
+    //recorrro hasta el \0
+    for(int i=0; cadena[i] != '\0';i++)
+    {
+        if(cadena[i]>=97 && cadena[i]<=122)//mayor o igual a 37
+        {
+            //contador[i]=toupper(cadena[i]);
+            contador[i]=cadena[i]-32;
+           // contador++;
+
+        }
+
+
+    }
+    return contador;
+
 }
