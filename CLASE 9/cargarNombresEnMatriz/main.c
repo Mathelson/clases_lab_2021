af55a9a08f89bf34512715c97123152a3a16dc28
unknown <PC2@T03-002.icecream.loc>
2021-04-19 15:12:59 -0300

    main ORDENA POR NOMBRE = NO FUNCION

--- CLASE 9/cargarNombresEnMatriz/main.c
+++ CLASE 9/cargarNombresEnMatriz/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #define FILAS 5
 #define COLUMNAS 20
 
@@ -50,4 +52,4 @@ int main()
     printf("\n\n");
     return 0;
 }
-void mostrarNombres(char names[])
+//void mostrarNombres(char names[])
